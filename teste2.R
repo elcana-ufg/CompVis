ef7f570879a806362c55a645e73d48e3315b76d8
elcana-ufg <104227291+elcana-ufg@users.noreply.github.com>
2022-09-09 09:13:35 -0300

    Update teste2.R

--- teste2.R
+++ teste2.R
@@ -1,37 +1,25 @@
-# load RData file a calculate the rate of reaction
+# load RData file and calculate the rate of reaction, k
 
-##
-## usando o modelo padrao ln (Aoo - At) ~ t
-## para ln(Soo - St) ~ t
-##
+load("Smean.RData")
+S = matrix(unlist(run), ncol = 1, nrow = length(run), byrow = TRUE)
 
-load("teste2_run10.RData")
-S10 = matrix(unlist(run10), ncol = 1, nrow = length(run10), byrow = TRUE)
+fps = 30 # video frame per second
+# S mean values during the time of reaction
+S.r = S[which.max(S):length(S)] 
+t.r = seq(0,(length(S)-which.max(S)))/fps
+# S min-max normalization
+process <- caret::preProcess(as.data.frame(S.r), method=c("range"))
+norm_scale <- predict(process, as.data.frame(S.r))
+S.n <- norm_scale$S.r
+# Ln(S)
+lnS = log( S.n - S.n[length(S.n)] )
+t_adj <- 180*30 # time of 180 s
+runln.fit <- lm(LnS[1:t_adj] ~ time[1:t_adj])
 
-
-# intervalos de medidas
-fps = 30 # depende do video
-S10.r = S10[which.max(S10):length(S10)]
-t10.r = seq(0,(length(S10)-which.max(S10)))/fps
-
-process <- caret::preProcess(as.data.frame(S10.r), method=c("range"))
-norm_scale <- predict(process, as.data.frame(S10.r))
-S10.n <- norm_scale$S10.r
-
-lnS10 = log( S10.n - S10.n[length(S10.n)] )
-
-#--------------------------------
-# de acordo com 
-# file:///home/elcana/grupo/isabella/docs/M9201_FadingPhenolphthalein.pdf
-# o ajuste para NaOH 0.3 M é 180 s
-
-t_ajustado <- 180*30
-runln10.fit <- lm(LnS[1:t_ajustado] ~ time[1:t_ajustado], data = runln10.df)
-
-k <- runln10.fit$coefficients[2]
+k <- runln.fit$coefficients[2]
               
-conc.NaOH <- 0.28
-slope_conc_10_12 <- k10_12/conc.NaOH # artigo 0.021
+cont.NaOH <- 0.28
+slope_cont <- k/cont.NaOH 
 
 
 
